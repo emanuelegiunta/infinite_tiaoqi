bbb13f94c6900ac92518e79ccdcd6fb5942eb838
Emanuele Giunta <emanuele.giunta@imdea.org>
2022-12-23 13:09:31 +0100

    Added tests for `move` and `paths`

--- test/test_pieces.py
+++ test/test_pieces.py
@@ -16,12 +16,15 @@ class TestGameState(unittest.TestCase):
     <    >  player_add()
     <    >  player_pop()
 
-    <    >  path(x, y)
+    <    >  paths(x, y)
     <    >  move(x1, y1, x2, y2)
     <    >  move_force(x1, y1, x2, y2)
     '''
+
     def setUp(self):
         self.gs = GameState()
+        self.NEIGHBOURHOOD = ((1, 0), (0, 1), (-1, 1), (-1, 0), (0, -1), 
+            (1, -1))
 
     def test_board_add(self):
         # setup the board
@@ -293,5 +296,428 @@ class TestGameState(unittest.TestCase):
                 self.assertEqual(self.gs.pieces, D)
                 self.assertEqual(self.gs.board, S)
                 self.assertEqual(self.gs.player_num, 2)
+
+    def test_paths_error(self):
+        # Empty Board
+        with self.subTest("out of board, empty board"):
+            self.assertRaises(ValueError, self.gs.paths, 0, 0)
+
+        # Filled board
+        with self.subTest("out of board, non-empty board"):
+            self.gs.board_add_iter(self.NEIGHBOURHOOD)
+            assert self.gs.board == set(self.NEIGHBOURHOOD)
+            self.assertRaises(ValueError, self.gs.paths, 0, 0)
+
+    def test_paths_empty(self):
+        # SETUP board and player
+        S = {(0, 0)}
+        self.gs.board_add_iter(S)
+        assert self.gs.board == S 
+        assert self.gs.pieces == {}
+        assert self.gs.player_num == 0
+
+        P = {(0, 0): None}
+
+        # No path if there is no available tile (with empty center)
+        with self.subTest("no piece"):
+            # P only has the "standing still" part of the path
+            self.assertEqual(self.gs.paths(0, 0), P)
+
+        # No path if there is no available tile (with empty center)
+        with self.subTest("with piece"):
+            self.gs.piece_add(0, 0, "j")
+            assert self.gs.pieces == {(0, 0): "j"}
+
+            self.assertEqual(self.gs.paths(0, 0), P)
+            # TeadDown the piece
+            self.gs.piece_remove(0, 0)
+            assert self.gs.pieces == {}
+
+        # SETUP add to board disconnected tiles at distance two
+        #   `.` = empty spot (no tile),
+        #   `o` = tile slot
+        #
+        #        o . o             
+        #       . . . .            
+        #      o . o . o            
+        #       . . . .            
+        #        o . o    
+        #         
+        S.union({(2*x, 2*y) for (x, y) in self.NEIGHBOURHOOD})
+        self.gs.board_add_iter(S)
+
+        assert self.gs.board == S
+        assert self.gs.pieces == {}
+        assert self.gs.player_num == 0
+
+        # No path, with no pieces
+        with self.subTest("no piece, far tiles"):
+            self.assertEqual(self.gs.paths(0, 0), P)
+
+        # No path, piece in the center
+        with self.subTest("with piece, far tiles"):
+            self.gs.piece_add(0, 0, "j")
+            assert self.gs.pieces == {(0, 0): "j"}
+
+            self.assertEqual(self.gs.paths(0, 0), P)
+            # TearDown the piece
+            self.gs.piece_remove(0, 0)
+            assert self.gs.pieces == {}
+
+    def test_paths_neighbouring(self):
+        # SETUP board and pieces
+        S = {(0, 0)}
+        self.gs.board_add_iter(S)
+        assert self.gs.board == S 
+        assert self.gs.pieces == {}
+        assert self.gs.player_num == 0
+
+        # SETUP paths
+        P = {(0, 0): None}
+
+        # TEST: adding tiles in the neighbourhood progressively and check
+        #  that path is correctly generated
+        #
+        # eg. at fourth iteration the state is
+        #
+        #          . .
+        # board =   . .
+        #            .
+        for tile in self.NEIGHBOURHOOD:
+            S.add(tile)
+            P[tile] = (0, 0)
+
+            # SETUP add the new tile
+            self.gs.board_add(*tile)
+            assert self.gs.board == S 
+            assert self.gs.pieces == {}
+            with self.subTest("partial neighbourhood", S=S):
+                
+                # No piece test
+                self.assertEqual(self.gs.paths(0, 0), P) 
+
+                # Piece test
+                self.gs.piece_add(0, 0, "j")
+                assert self.gs.pieces == {(0, 0): "j"}    
+                self.assertEqual(self.gs.paths(0, 0), P)
+                self.gs.piece_remove(0, 0)
+                assert self.gs.pieces == {}
+
+        # TEST: filling up tiles in the neighbourhood with pieces and check
+        #  that path is correctly generated
+        #
+        # eg. at fourth iterarion the state is
+        #
+        #          u u
+        # board = . . u
+        #          . u
+        #
+        D = {}
+        for tile in self.NEIGHBOURHOOD:
+            D[tile] = "u"
+            P.pop(tile)
+            self.gs.piece_add(*tile, "u")
+            assert self.gs.board == S 
+            assert self.gs.pieces == D
+
+            with self.subTest("filled neighbourhood", D=D):
+
+                # No piece test
+                self.assertEqual(self.gs.paths(0, 0), P)
+
+                # Piece test
+                self.gs.piece_add(0, 0, "j")
+                D[(0, 0)] = "j"
+                assert self.gs.pieces == D
+                self.assertEqual(self.gs.paths(0, 0), P)
+                self.gs.piece_remove(0, 0)
+                D.pop((0, 0))
+                assert self.gs.pieces == D
+
+    def test_paths_no_cycles(self):
+        # SETUP: radius 2 board
+        #
+        #           . . . 
+        #          . o o . 
+        # board = . o . o .
+        #          . o o . 
+        #           . . . 
+        #
+        # Note: we use this setup because no cycles are possibile, thus the
+        #  paths tree is unique, so we can compare the output against the
+        #  only possibile solution (as opposed to list all possibile solutions)
+        S = {(0, 0)}
+        S = {(x+dx, y+dy) for (x, y) in S for (dx, dy) in self.NEIGHBOURHOOD}
+        S = {(x+dx, y+dy) for (x, y) in S for (dx, dy) in self.NEIGHBOURHOOD}
+        self.gs.board_add_iter(S)
+        for (x, y) in self.NEIGHBOURHOOD:
+            self.gs.piece_add(x, y, "j")
+        D = {p: "j" for p in self.NEIGHBOURHOOD}
+        assert len(S) == 19
+        assert self.gs.board == S 
+        assert self.gs.pieces == {p: "j" for p in self.NEIGHBOURHOOD} 
+        assert self.gs.player_num == 0
+
+        # Test paths from (-1, 2)
+        #
+        #           . . . 
+        #          . o o x 
+        # board = . o . o .
+        #          . o o . 
+        #           . . . 
+        #
+
+        P = {(-1, 2): None, (0, 2): (-1, 2), (-2, 2): (-1, 2)}
+        with self.subTest("p = (-1, 2)"):
+            self.assertEqual(self.gs.paths(-1, 2), P)
+
+        # Test paths from (0, 0)
+        #
+        #           . . . 
+        #          . o o . 
+        # board = . o x o .
+        #          . o o . 
+        #           . . . 
+        #
+
+        P = {(0, 0): None, (2, 0): (0, 0), (0, 2): (0, 0), (-2, 2): (0, 0),
+             (-2, 0): (0, 0), (0, -2): (0, 0), (2, -2): (0, 0)}
+        with self.subTest("p = (0, 0)"):
+            self.assertEqual(self.gs.paths(0, 0), P)
+
+        # Test paths from (2, 0)
+        #
+        #           x . . 
+        #          . o o . 
+        # board = . o . o .
+        #          . o o . 
+        #           . . . 
+        #
+
+        P = {(2, 0): None, (0, 0): (2, 0), (0, 2): (0, 0), (-2, 2): (0, 0),
+             (-2, 0): (0, 0), (0, -2): (0, 0), (2, -2): (0, 0), (1, 1): (2, 0),
+             (2, -1): (2, 0)}
+        with self.subTest("p = (2, 0)"):
+            self.assertEqual(self.gs.paths(2, 0), P)
+
+        # Test paths from (1, -1)
+        #
+        #           . . . 
+        #          . o o . 
+        # board = . x . o .
+        #          . o o . 
+        #           . . . 
+        #
+
+        P = {(1, -1): None, (2, -1): (1, -1), (0, 0): (1, -1), 
+             (1, -2): (1, -1), (2, -2): (1, -1), (1, 1): (1, -1), 
+             (-1, -1): (1, -1)}
+        with self.subTest("p = (1, -1)"):
+            self.assertEqual(self.gs.paths(1, -1), P)
+
+    def test_paths_cycles(self):
+        # SETUP: radius 1 diamond
+        #
+        #           .          
+        #          o o
+        # board = . . .
+        #          o o
+        #           .
+        # 
+        S = {(x,y) for x in range(-1, 2) for y in range(-1, 2)}
+        self.gs.board_add_iter(S)
+
+        # SETUP: two lines of pieces
+        l = ((1, 0), (0, 1), (-1, 0), (0, -1))
+        for (x, y) in l:
+            self.gs.piece_add(x, y, "j")
+        D = {p: "j" for p in l}
+
+        assert self.gs.board == S 
+        assert self.gs.pieces == {p: "j" for p in l} 
+        assert self.gs.player_num == 0
+
+        # TEST: paths from (1, -1)
+        #
+        #           .
+        #          o o
+        # board = x . .
+        #          o o
+        #           .
+
+        # P_base, part of the tree shared among all possibile paths
+        P_base = {(1, -1): None, (0, 0): (1, -1), (1, 1): (1, -1), 
+                  (-1, -1): (1, -1)}
+
+        # P1, assuming (-1, 1) is reached from (1, 1)
+        P1 = P_base.copy()
+        P1[(-1, 1)] = (1, 1)
+
+        # P2, assuming (-1, 1) is reached from (-1, -1)
+        P2 = P_base.copy()
+        P2[(-1, 1)] = (-1, -1)
+
+        self.assertTrue(self.gs.paths(1, -1) in (P1, P2))
+
+    def test_paths_unjumpable(self):
+        # SETUP: 3 tiles line
+        #
+        #         . 
+        # board =  u
+        #           .
+        #
+        S = {(i, 0) for i in range(-1, 2)}
+        self.gs.board_add_iter(S)
+        self.gs.piece_add(0, 0, "u")
+
+        assert self.gs.board == S
+        assert self.gs.pieces == {(0, 0): "u"}
+        assert self.gs.player_num == 0
+
+        self.assertEqual(self.gs.paths(-1, 0), {(-1, 0): None})
+
+
+    def test_move_errors(self):
+        # SETUP: radius 1 ball
+        #
+        #          u j 
+        # board = . . 0
+        #          . 1
+        #
+        # u: unjump, j: jump, 0: player 0, 1: player 1
+
+        S = {(0, 0)}.union(set(self.NEIGHBOURHOOD))
+        self.gs.board_add_iter(S)
+        self.gs.player_add(2)
+        self.gs.piece_add(1, 0, "u")
+        self.gs.piece_add(0, 1, "j")
+        self.gs.piece_add(-1, 1, "0")
+        self.gs.piece_add(-1, 0, "1")
+
+        assert self.gs.board == S 
+        assert self.gs.pieces == {(1, 0): "u", (0, 1): "j", (-1, 1): "0",
+                                  (-1, 0): "1"}
+        assert self.gs.player_num == 2
+
+        # TEST: moving a tile not containing a `current round piece`
+        # p = (1, 0) -> move an unjump
+        # p = (0, 1) -> move a non-playing jump
+        # p = (-1, 0) -> move a piece of a player in the wrong round
+        #
+        # Note: for all this piece (0, 0) is a valid destination
+        for (x, y) in ((1, 0), (0, 1), (-1, 0)):
+            with self.subTest("wrong piece", piece=self.gs.pieces[(x, y)]):
+                self.assertRaises(ValueError, self.gs.move, x, y, 0, 0)
+
+        # TEST: no path & move in place
+        #
+        # We iterate on all board point but the center since
+        # from (-1, 1) the center is reacheable
+        for (x, y) in S.difference({(0, 0)}):
+            with self.subTest("no path", point=(x, y)):
+                self.assertRaises(ValueError, self.gs.move, -1, 1, x, y)
+
+    def test_move_3_players(self):
+        # SETUP: radius 2 ball
+        #
+        #           . 0 . 
+        #          . . 1 2 
+        # board = . . 1 . .
+        #          . 0 2 . 
+        #           . . .  
+        # 
+        S = {(0, 0)}
+        S = {(x+dx, y+dy) for (x, y) in S for (dx, dy) in self.NEIGHBOURHOOD}
+        S = {(x+dx, y+dy) for (x, y) in S for (dx, dy) in self.NEIGHBOURHOOD}
+        self.gs.board_add_iter(S)
+        self.gs.player_add(3)
+        self.gs.piece_add(1, 1, "0")
+        self.gs.piece_add(0, -1, "0")
+        self.gs.piece_add(0, 1, "1")
+        self.gs.piece_add(0, 0, "1")
+        self.gs.piece_add(-1, 2, "2")
+        self.gs.piece_add(-1, 0, "2")
+
+        D = {(1, 1): "0", (0, -1): "0",
+            (0, 1): "1", (0, 0): "1",
+            (-1, 2): "2", (-1, 0): "2",
+        }
+
+        assert len(S) == 19
+        assert self.gs.board == S
+        assert self.gs.pieces == D
+
+        # STATUS after [player 0] : (1, 1) -> (-1, -1)
+        #
+        #           . . . 
+        #          . . 1 2 
+        # board = . . 1 . .
+        #          . 0 2 . 
+        #           . 0 . 
+        # 
+        with self.subTest("first move"):
+            P = self.gs.move(1, 1, -1, -1)
+            # Note: there are several paths leadind to (-1, -1) but this is the
+            #       only one of length 2. Note the API ensures the returned
+            #       path as to be the shortest.
+            #
+            self.assertEqual(tuple(P), ((1, 1), (-1, 1), (-1, -1)))
+            # Check the round was correctly passed
+            self.assertEqual(self.gs.player, 1)
+            # Check pieces were correctly moved
+            D.pop((1, 1))
+            D[(-1, -1)] = "0"
+            self.assertEqual(self.gs.pieces, D)
+
+        # STATUS after [player 1] : (0, 0) -> (-2, 2)
+        #
+        #           . . . 
+        #          . . 1 2 
+        # board = . . 1 . .
+        #          . 0 2 . 
+        #           . 0 . 
+        #
+        with self.subTest("second move"):
+            P = self.gs.move(0, 0, -2, 2)
+            D.pop((0, 0))
+            D[(-2, 2)] = "1"
+
+            self.assertEqual(tuple(P), ((0, 0), (0, 2), (-2, 2)))
+            self.assertEqual(self.gs.player, 2)
+            self.assertEqual(self.gs.pieces, D)
+
+        # STATUS after [player 1] : (-1, 0) -> (-1, 1)
+        #
+        #           . . . 
+        #          . . 1 2 
+        # board = . . 1 2 .
+        #          . 0 . . 
+        #           . 0 . 
+        #
+        with self.subTest("third move"):
+            P = self.gs.move(-1, 0, -1, 1)
+            D.pop((-1, 0))
+            D[(-1, 1)] = "2"
+
+            self.assertEqual(tuple(P), ((-1, 0), (-1, 1)))
+            # Since we have two players, the player counter should wrap to 0
+            self.assertEqual(self.gs.player, 0)
+            self.assertEqual(self.gs.pieces, D)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
